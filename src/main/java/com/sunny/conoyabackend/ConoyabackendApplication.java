11862e748b5f9f8f061adf72ce2440450834244c
p614080 <p614080@naver.com>
2024-09-30 16:31:34 +0900

    Initial commit

---
+++ src/main/java/com/sunny/conoyabackend/ConoyabackendApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.conoyabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConoyabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConoyabackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunny/conoyabackend/ConoyabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.conoyabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConoyabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
