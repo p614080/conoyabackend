fca86f3f9b98eea8c0415e1101e80ef949f0fda8
rudgh9290 <ossetian@naver.com>
2024-10-01 14:23:18 +0900

    gyeonghotest2

---
+++ src/main/java/com/sunny/conoyabackend/Entity/Owner.java
@@ -0,0 +1,30 @@
+package com.sunny.conoyabackend.Entity;
+
+
+import com.sunny.conoyabackend.Role;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Owner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ownerId;
+
+    private String ownerEmail;
+    private String ownerPassword;
+    private String ownerNickname;
+
+    private Role role;
+}
