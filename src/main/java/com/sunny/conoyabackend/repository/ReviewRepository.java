c1f034d1fcc2298b2aa3832b1a0f8a611f5539ec
p614080 <p614080@naver.com>
2024-10-07 14:47:06 +0900

    test

--- src/main/java/com/sunny/conoyabackend/repository/ReviewRepository.java
+++ src/main/java/com/sunny/conoyabackend/repository/ReviewRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findAll(Pageable pageable);
+
+    List<Review> findAllByOwnerEntity_OwnerId(long l);
 }
---
+++ src/test/java/com/sunny/conoyabackend/repository/ReviewCommentRepositoryTests.java
@@ -0,0 +1,10 @@
+package com.sunny.conoyabackend.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ReviewCommentRepositoryTests {
+
+}
