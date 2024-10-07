9c97001d9bacde1cf52200e37f7591c9d6a2cedc
rudgh9290 <ossetian@naver.com>
2024-10-07 17:07:53 +0900

    gyeonghotest

--- src/main/java/com/sunny/conoyabackend/repository/OwnerRepository.java
+++ src/main/java/com/sunny/conoyabackend/repository/OwnerRepository.java
@@ -3,10 +3,11 @@ package com.sunny.conoyabackend.repository;
 import com.sunny.conoyabackend.entity.OwnerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OwnerRepository extends JpaRepository<OwnerEntity, Long> {
     boolean existsByOwnerEmail(String ownerEmail);
-    boolean existsByStoreName(String storeName);
     Optional<OwnerEntity> findByOwnerEmail(String ownerEmail);
+    List<OwnerEntity> findAll();
 }
