0b65b0d3afe021310f0efab40f382ae8aae14583
p614080 <p614080@naver.com>
2024-10-08 11:03:55 +0900

    test

--- src/main/java/com/sunny/conoyabackend/dto/RoomDTO.java
+++ src/main/java/com/sunny/conoyabackend/dto/RoomDTO.java
@@ -1,23 +1,25 @@
 package com.sunny.conoyabackend.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class RoomDTO {
-    
+
     private Long roomId; // 방 ID
     private int roomNumber; // 방 번호
     private boolean useroom; // 방 사용 여부
-    private int roomposition; // 방 최대 인원
-    private int remainingTime; // 남은 시간 (분 단위)
-    private int remainingSongs; // 남은 곡 수
-    private double roomRate; // 방 요금
+    private int roomSize; // 방 최대 인원
+    private LocalDateTime startTime; // 시작 시간
+    private int paymentTime; // 시간으로 결제
+    private int paymentCoin; // 곡 수로 결제
+    private String roomRate; // 방 요금
+    private List<ImageDTO> images; // 이미지 목록
+    private OwnerDTO ownerEntity; // 점주 정보
 }
