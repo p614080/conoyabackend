e151c86b12578a405889d42034466683cfcb86d0
p614080 <p614080@naver.com>
2024-10-08 11:32:33 +0900

    test

--- src/main/java/com/sunny/conoyabackend/service/RoomServiceImpl.java
+++ src/main/java/com/sunny/conoyabackend/service/RoomServiceImpl.java
@@ -1,5 +1,7 @@
 package com.sunny.conoyabackend.service;
 
+import com.sunny.conoyabackend.dto.RoomDTO;
+import com.sunny.conoyabackend.repository.RoomRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,22 @@ import org.springframework.stereotype.Service;
 @Transactional
 @RequiredArgsConstructor
 public class RoomServiceImpl implements RoomService{
+    private final RoomRepository roomRepository;
+
+    @Override
+    public Long register(RoomDTO roomDTO){
+
+        return 0L;
+    }
+
+    @Override
+    public RoomDTO get(Long roomId){
+
+        return null;
+    }
+
+    @Override
+    public void remove(Long roomId){
+
+    }
 }
